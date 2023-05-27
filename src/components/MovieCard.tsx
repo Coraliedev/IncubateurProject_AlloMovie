c90e77779fb7776df23899c20313320fb919e5b8
CoralieDev <coralie.acquaert@gmail.com>
2023-05-27 21:14:42 +0200

    feat(movies): add color to note for movies

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -5,15 +5,26 @@ import { HeartIcon, EyeIcon } from "@heroicons/react/20/solid"
 
 const MovieCard = ({ movie }: { movie: MovieModel }) => {
 
-
+const setColorVote = (vote: number) => {
+    if (vote >= 8) {
+      return "bg-green-500"
+    } else if (vote >= 6.5) {
+      return "bg-yellow-500"
+    } else if (vote >= 5) {
+      return "bg-orange-500"
+    }
+    else {
+      return "bg-red-500"
+    }
+  }
   return (
-      <div className={`rounded-xl relative text-gray-400 font-bold hover:scale-110`} >
+      <div className="rounded-xl relative text-gray-100 font-bold hover:scale-110" >
         <img
           src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
           alt={movie.title + ' poster'}
           className="rounded-xl h-full"
         />
-        <p className="flex justify-center items-center rounded-tl-xl rounded-br-lg absolute top-0 left-0 w-10 h-10">{movie.vote_average}</p>
+        <p  className={`${setColorVote(movie.vote_average)} flex justify-center items-center rounded-tl-xl rounded-br-lg absolute top-0 left-0 w-10 h-10`}>{movie.vote_average}</p>
          <HeartIcon className="absolute top-0 right-1 w-10 h-10 text-gray-400 hover:text-red-500"/>
          <Link to={`/details/${movie.id}`}  >
           <EyeIcon className="absolute bottom-1 right-1 w-10 h-10 text-gray-400"/>
