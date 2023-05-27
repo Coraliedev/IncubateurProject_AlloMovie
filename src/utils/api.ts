869284d0c36111493514cda3b332ca9a321c4a55
CoralieDev <coralie.acquaert@gmail.com>
2023-05-27 20:47:57 +0200

    feat: create a file with data for API requests

---
+++ src/utils/api.ts
@@ -0,0 +1,4 @@
+export const API_KEY: string = import.meta.env.VITE_API_KEY
+export const MOVIE_API = "https://api.themoviedb.org/3/"
+export const SEARCH_API = MOVIE_API + "search/movie"
+export const DISCOVER_API = MOVIE_API + "discover/movie"
