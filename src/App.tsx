060a50640da1506a930500577f37f4c4b25509c1
CoralieDev <coralie.acquaert@gmail.com>
2023-05-27 16:08:37 +0200

    feat(react-query): configure react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css'
 import Routes from './routes/index'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient();
 
 const App: React.FC = () => {
   return (
+    <QueryClientProvider client={queryClient}>
     <div className="app h-screen">
       <Routes />
     </div>
+    <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 
