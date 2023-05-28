4f34fb5634b6fb4f3e14818a1c003ba4e2a43a6f
CoralieDev <coralie.acquaert@gmail.com>
2023-05-28 11:28:33 +0200

    fix: add content when data is loading and when no movies found with the search key

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -28,25 +28,31 @@ const Home: React.FC = () => {
   }
 
   // fetch and cache movies with react-query
-  useQuery<MovieModel[]>({
+  const { isLoading } = useQuery<MovieModel[]>({
     queryKey: ['movie-list', page, searchKey],
     queryFn: fetchMovies,
     refetchOnWindowFocus: false,
     onSuccess: (res) => {
       setMovies(res)
-    },
+    }
   })
 
 
   return (
     <>
-    <SearchBar setSearchKey={setSearchKey}/>
-        <div className="dark:bg-gray-900 py-12 mx-auto md:px-12 px-8 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-x-16 gap-y-16">
-          {movies.map((movie: MovieModel) => (
-            <MovieCard movie={movie} key={movie.id} />
-          ))}
-        </div>
-      <Pagination page={page} setPage={setPage} />
+      <SearchBar setSearchKey={setSearchKey} />
+      {isLoading ? <p className="dark:bg-gray-900 text-gray-100 font-bold text-gray-100 font-bold flex justify-center items-center text-3xl md:text-4xl lg:text-6xl">Loading...</p> :
+        movies.length < 1 ?
+          <h1 className="dark:bg-gray-900 min-h-[80%] text-gray-100 font-bold flex justify-center items-center text-3xl md:text-4xl lg:text-6xl">No movies found</h1> :
+          <>
+            <div className="dark:bg-gray-900 py-12 mx-auto md:px-12 px-8 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-x-16 gap-y-16">
+              {movies.map((movie: MovieModel) => (
+                <MovieCard movie={movie} key={movie.id} />
+              ))}
+            </div>
+            
+          </>}
+          <Pagination page={page} setPage={setPage} />
     </>
   )
 }
