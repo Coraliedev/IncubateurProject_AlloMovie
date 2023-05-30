938b6027ca5608b4d351eb2c24f38656d525525e
CoralieDev <coralie.acquaert@gmail.com>
2023-05-30 11:37:46 +0200

    feat(auth): create context for modalAuth visibility

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,22 @@ import './App.css'
 import Routes from './routes/index'
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { auth } from './firebase'
-import { signInWithEmailAndPassword } from 'firebase/auth'
-import { useEffect } from 'react'
+import { useState } from 'react'
+import AuthModalContext from './context/AuthModal'
 
 const queryClient = new QueryClient();
 
 const App: React.FC = () => {
-
-  useEffect (() => {
-    signInWithEmailAndPassword(auth, "coral@gmail.com", "coralie" );
-  }, [])
+  const [modalVisibility, setModalVisibility] = useState("hidden");
 
   return (
     <QueryClientProvider client={queryClient}>
-    <div className="app h-screen max-w-screen dark:bg-gray-900">
-      <Routes />
-    </div>
-    <ReactQueryDevtools initialIsOpen={false} />
+      <div className="app h-screen max-w-screen dark:bg-gray-900">
+        <AuthModalContext.Provider value={{ modalVisibility, setModalVisibility }}>
+          <Routes />
+        </AuthModalContext.Provider>
+      </div>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
 }
