0d48514cb458944d8e74f92e15b224bc60000154
CoralieDev <coralie.acquaert@gmail.com>
2023-05-30 19:00:28 +0200

    feat(auth): create login form component

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,6 +1,51 @@
+import { useContext } from "react";
+import AuthFirebaseContext from "../context/AuthFirebaseContext";
+import AuthModalContext from "../context/AuthModalContext";
+import LoginFormModel from "../models/LoginForm.model";
+import * as Yup from 'yup';
+import { ErrorMessage, Field, Form, Formik } from "formik";
+
 const LoginForm = () => {
+  const { login } = useContext(AuthFirebaseContext);
+   const {setModalVisibility } = useContext(AuthModalContext);
+
+   const LoginSchema = Yup.object().shape({
+    email: Yup.string().email('Invalid email').required('Email is required'),
+    password: Yup.string().required('Password is required'),
+  });
+
+   const handleLogin = async (values: LoginFormModel) => {
+    const { email, password, } = values;
+    try {
+      await login(email, password);
+      setModalVisibility("hidden");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
   return (
-    <h1>LoginForm</h1>
+    <Formik
+    initialValues={{ email: '', password: '' }}
+    validationSchema={LoginSchema}
+    onSubmit={handleLogin}
+  >
+    <Form>
+      <div>
+        <label htmlFor="email">Email</label>
+        <Field type="email" name="email" />
+        <ErrorMessage name="email" component="div" />
+      </div>
+
+      <div>
+        <label htmlFor="password">Password</label>
+        <Field type="password" name="password" />
+        <ErrorMessage name="password" component="div" />
+      </div>
+
+      <button type="submit">Login</button>
+    </Form>
+  </Formik>
   )
 }
 
---
+++ src/models/LoginForm.model.ts
@@ -0,0 +1,4 @@
+export default interface LoginFormModel {
+    email: string;
+    password: string;
+}
