6444de46b13b0e2605a3dd8fb553374604a91e1d
CoralieDev <coralie.acquaert@gmail.com>
2023-07-21 15:45:59 +0200

    feat(auth): check at the opening if user is alrealdy connected for user/logout icon

--- src/services/firebase.service.tsx
+++ src/services/firebase.service.tsx
@@ -6,10 +6,23 @@ import {
 } from "firebase/auth";
 import { authVisibilityAtom, isConnectedAtom } from "../atoms";
 import { doc, setDoc } from "firebase/firestore";
+import { useEffect } from "react";
 
 export const useFirebaseAuth = () => {
   const [isConnected, setIsConnected] = useAtom(isConnectedAtom)
   const [authVisibility, setAuthVisibility] = useAtom(authVisibilityAtom)
+
+  useEffect (() => {
+ // check if user is connected or not at opening of the app
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setIsConnected(true)
+      } else {
+        setIsConnected(false)
+      }
+    }
+    )
+  }, [setIsConnected])
 
   const login = async (email: string, password: string) => {
     await signInWithEmailAndPassword(auth, email, password);
